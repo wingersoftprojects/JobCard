a0d16fc08cc042d3fea908d94d5d28737e0147ce
bajuna <bajuna@NHQ-WS-019.ura.local>
2017-03-01 21:24:55 +0300

    Worked on the view for Job Card

--- src/java/beans/Job_cardBean.java
+++ src/java/beans/Job_cardBean.java
@@ -17,6 +17,7 @@ import models.Customer_detail;
 import models.JobCardPersistentManager;
 import models.Job_card;
 import models.Job_card_item;
+import models.Paper_type;
 import models.Supplier_detail;
 import org.orm.PersistentException;
 
@@ -86,6 +87,15 @@ public class Job_cardBean extends AbstractBean<Job_card> implements Serializable
             Logger.getLogger(Customer_detailBean.class.getName()).log(Level.SEVERE, null, ex);
         }
         return filteredSupplier_details;
+    }
+    public List<Paper_type> completePaper_type(String query) {
+        List<Paper_type> filteredPaper_types = new ArrayList<>();
+        try {
+            filteredPaper_types = (List<Paper_type>) JobCardPersistentManager.instance().getSession().createQuery("select de FROM Paper_type  de where de.is_deleted<>1 AND ( de.paper_type_category like '%" + query + "%' OR  de.paper_type_sub_category like '%" + query + "%')").list();
+        } catch (PersistentException ex) {
+            Logger.getLogger(Customer_detailBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return filteredPaper_types;
     }
 
     public void add_job_card_item() {
