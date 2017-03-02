d95478f7b712a3c9166e16b869ce1a2bfedac224
bajuna <bajuna@NHQ-WS-019.ura.local>
2017-03-02 18:12:38 +0300

    Added Cover typeBean

---
+++ src/java/beans/Cover_typeBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import models.Cover_type;
+
+/**
+ *
+ * @author bajuna
+ */
+@ManagedBean
+@SessionScoped
+public class Cover_typeBean extends AbstractBean<Cover_type> implements Serializable {
+
+    public Cover_typeBean() {
+        super(Cover_type.class);
+    }
+
+    @Override
+    public void init() {
+        if (super.getEntityClass() == null) {
+            loginBean.logout();
+        }
+    }
+    @ManagedProperty("#{loginBean}")
+    private LoginBean loginBean;
+
+    public LoginBean getLoginBean() {
+        return loginBean;
+    }
+
+    public void setLoginBean(LoginBean loginBean) {
+        this.loginBean = loginBean;
+    }
+
+}
